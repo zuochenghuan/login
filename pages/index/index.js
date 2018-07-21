64858f38fb7271502e3f65a36e2d973343f77340
zch199383 <2685119427@qq.com>
2018-07-21 13:55:07 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,21 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000
+  },
+  onLoad: function (options) {
+    
+  }
+})
